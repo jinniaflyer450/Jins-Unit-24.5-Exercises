8197d755eb12674418c153aa9d0f40250a9adc10
Elizabeth Watson <elizabethwats0n450@gmail.com>
2021-09-20 23:20:34 -0500

    completed logic for '/' and '/register' routes

--- app.py
+++ app.py
@@ -18,8 +18,27 @@ connect_db(app)
 db.create_all()
 
 @app.route('/')
+def redirect_register():
+    return redirect('/register')
 
 @app.route('/register', methods=["GET", "POST"])
+def register_user():
+    form = RegisterUserForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        email = form.email.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        
+        new_user = User.register(username=username, password=password, email=email, 
+        first_name=first_name, last_name=last_name)
+        
+        db.session.add(new_user)
+        db.session.commit()
+        flash("Successfully registered!")
+        return redirect('/secret')
+    return render_template('register.html', form=form)
 
 @app.route('/login', methods=["GET", "POST"])
 
